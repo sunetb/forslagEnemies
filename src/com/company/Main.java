58f639e62e4b2aad77aaecc3ea5b4fa4c4dd15a7
sune <sunetb@gmail.com>
2021-11-24 11:43:34 +0100

    Kommentar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,14 @@ package com.company;
 
 import java.util.Random;
 
+//Dette er en skitse/et fundament som vi har talt os frem til under projekthjælp
+//Byg gerne videre på det
+
 public class Main {
 
     public static void main(String[] args) {
 	Game g = new Game();
+	g.opdater();
     }
 }
 
@@ -14,10 +18,12 @@ class Game {
 
     int boardsize = 10;
 
-    Board b = new Board(boardsize);
+    Board board = new Board(boardsize);
 
     //opret dyreliste
     Animal[] dyreliste = new Animal[8];
+    // mangler obstacles
+    // player
 
     Game (){
 
@@ -38,30 +44,68 @@ class Game {
         for (Animal a : dyreliste){
             System.out.println(a.x + ":"+a.y);
         }
+        opdater();
 
     }
 
+    public void opdater() {
+
+        //brug et loop
+        Animal a  = dyreliste[0];
+        board.spilleplade[a.x][a.y] = 'A';
+        board.visBræt();
+
+
+    }
 }
-class Board{
+
+
+class Board {
     int boardsize;
 
-    char [][] spilleplade = new char[boardsize][boardsize];
+    char[][] spilleplade;
 
-    Board (int boardsize){
+    Board(int boardsize) {
+
         this.boardsize = boardsize;
-    }
+        spilleplade= new char[boardsize][boardsize];
+        nulstilBræt();
+        visBræt();
 
+    }
+        //Metode som inialiserer board: sætter alle felter til '_'
+
+
+    void nulstilBræt () {
+        for (int x = 0; x < boardsize; x++) {
+            for (int y = 0; y < boardsize; y++) {
+                spilleplade[x][y] = '_';
+            }
+        }
+    }
+    //Metode som printer boardet
+    void visBræt () {
+        for (int x = 0; x < boardsize; x++) {
+            for (int y = 0; y < boardsize; y++) {
+                System.out.print(spilleplade[y][x] + " ");
+            }
+            System.out.println();
+        }
+    }
+    //opdater
 
 
 }
 
  class   Obstacle{
-
+    int x,y;
  }
 
 
  class MainCharacter{
-
+    int x,y;
+    //flyt men på en anden måde en fjender: Brugeren styrer
+     //kollision og død
  }
 
 
@@ -69,4 +113,12 @@ class Animal {
     int x;
     int y;
 
+    void flyt(int spillerx, int spillery){
+        //hvis spillerx < x -> træk en fra x
+        //osv
+
+
+    }
+//flyt: 1 i retning af player
+    //kollision og død
 }
